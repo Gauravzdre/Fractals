f1544d391c14cd8cdf170dd773ef460d884993b5
Gauravzdre <31221637+Gauravzdre@users.noreply.github.com>
2019-12-12 22:08:12 +0530

    mandelbrot fractal

---
+++ Assets/MandelBrot/MandleBotExplorer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MandleBotExplorer : MonoBehaviour {
+
+    public Material m_Material;
+    public Vector4 m_Area;
+    [SerializeField] float moveSpeed;
+
+    private void Awake() {
+        m_Area = m_Material.GetVector("_Area");
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.KeypadPlus)) {
+            m_Area.z += moveSpeed;
+            m_Area.w += moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.KeypadMinus)) {
+            m_Area.z -= moveSpeed;
+            m_Area.w -= moveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W)) 
+            m_Area.y -= moveSpeed;
+        if (Input.GetKey(KeyCode.S))
+            m_Area.y += moveSpeed;
+        if (Input.GetKey(KeyCode.A)) 
+            m_Area.x -= moveSpeed;
+        if (Input.GetKey(KeyCode.D))
+            m_Area.x += moveSpeed;
+        Debug.Log(m_Area);
+
+        m_Material.SetVector("_Area", m_Area);
+    }
+}
